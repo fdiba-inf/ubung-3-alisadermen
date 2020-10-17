c45986cc26804478d821cfc89fb9b9630d0ab5a8
alisadermen <alisadermen@gmail.com>
2020-10-17 15:35:45 +0000

    Übung 2

--- src/exercise3/NumberCounter.java
+++ src/exercise3/NumberCounter.java
@@ -1,28 +1,32 @@
 package exercise3;
 import java.util.Scanner;
 public class NumberCounter {
-  public static void main (String[] args) {
-    Scanner input = new Scanner(System.in);
-    int pos = 0;
-    int neg = 0;
-    double sum = 0;
-    double average = 0;
-    int number = input.nextInt();
-    while (number != 0) {
-      if (number>0) {
-        pos++;
-      } else {
-        neg++;
-      }
-      sum += number;
-      average += sum / (pos + neg);
-    
-    }
-    System.out.println("Postive numbers; " + pos);
-    System.out.println("Negative numbers: " + neg);
-    System.out.println("Sum: " + sum);
-    System.out.println("Average: " + average);
-    
+  public static void main (String[] args){
+Scanner input = new Scanner(System.in);
+int number = input.nextInt();
+int positive = 0;
+int negative = 0;
+double sum = 0;
+double average = 0;
+
+ 
+while (number != 0) {
+ 
+  if  (number >0) {
+    positive++;
 
   }
+  else{
+    
+    negative++;
+
+  }
+  sum += number;
+  number = input.nextInt();
 }
+  System.out.println("Negative numbers: " + negative);
+  System.out.println("Positive numbers: " + positive);
+System.out.println("Sum: " + sum);
+System.out.println("Average: " + sum/(positive+negative));
+  }
+  }
